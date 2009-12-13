6254de46a64617a7c215826b24c2f55e66f59105
ciri@VirtuaX.be <ciri@VirtuaX.be@cf94e570-ce05-11de-958f-af26545d0b1a>
2009-12-13 04:09:14 +0000

    uber grote change not tested

--- trunk/Medicare/Medicare-ejb/src/java/ejb/patient/PatientBean.java
+++ trunk/Medicare/Medicare-ejb/src/java/ejb/patient/PatientBean.java
@@ -1,6 +1,5 @@
 package ejb.patient;
 
-import entity.measurement.MeasurementFacadeLocal;
 import entity.patient.PatientFacadeLocal;
 import entity.task.TaskFacadeLocal;
 import java.util.ArrayList;
@@ -25,70 +24,42 @@ public class PatientBean implements PatientRemote {
     private PatientFacadeLocal patientFacadeBean;
 
     @EJB
-    private MeasurementFacadeLocal measurementFacadeBean;
-
-    @EJB
     private TaskFacadeLocal taskFacadeBean;
     
-    public boolean addPatient(String SSN, String username, String password) {
-        try {
-            patientFacadeBean.createPatient(SSN, username, password);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-    public TaskDetails getTask(String username, String password) {
-        if(patientFacadeBean.isValid(username,password)) {
-            List<TaskDetails> task_aggregate = new ArrayList<TaskDetails>();
+    public TaskDetails getTask(String username) {
+        List<TaskDetails> task_aggregate = new ArrayList<TaskDetails>();
             
-            PatientDetails pd = patientFacadeBean.getPatient(username);
+        PatientDetails pd = patientFacadeBean.getPatient(username);
 
-            Collection<PrescriptionDetails> prescriptions = pd.getPrescriptions();
-            if(prescriptions != null)
-             for(PrescriptionDetails prescription :  prescriptions) {
-                Collection<TaskDetails> tasks = prescription.getTasks();
-                if(tasks != null)
-                 for(TaskDetails task : tasks) {
-                    if(task.getStatus() != null && task.getStatus().equals("in progress")) {
-                        task.setPrescription(prescription);
-                        task_aggregate.add(task);
-                    }
-                 }
-            }
-            if(!task_aggregate.isEmpty()) {
-                Collections.sort(task_aggregate);
-                TaskDetails curr_task = task_aggregate.get(0);
-                curr_task.setDose(curr_task.getPrescription().getMedication().getStandarddose());
-                return curr_task;
-            }
-            return null;
+        Collection<PrescriptionDetails> prescriptions = pd.getPrescriptions();
+        if(prescriptions != null)
+        for(PrescriptionDetails prescription :  prescriptions) {
+            Collection<TaskDetails> tasks = prescription.getTasks();
+            if(tasks != null)
+            for(TaskDetails task : tasks) {
+                if(task.getStatus() != null && task.getStatus().equals("in progress")) {
+                    task.setPrescription(prescription);
+                    task_aggregate.add(task);
+                }
+             }
+        }
+        if(!task_aggregate.isEmpty()) {
+            Collections.sort(task_aggregate);
+            TaskDetails curr_task = task_aggregate.get(0);
+            curr_task.setDose(curr_task.getPrescription().getMedication().getStandarddose());
+            return curr_task;
         }
         else
             return null;
     }
-    public boolean updateTask(String username, String password, String taskid, String newstatus) {
-        if(patientFacadeBean.isValid(username,password))
-            return taskFacadeBean.updateTask(Long.parseLong(taskid),newstatus);
-        else
-            return false;
+    public boolean updateTask( String taskid, String newstatus ) {
+        return taskFacadeBean.updateTask(Long.parseLong(taskid),newstatus);
     }
-    public PatientDetails getPatientDetails(String username, String password) {
-        if(patientFacadeBean.isValid(username,password))
-            return patientFacadeBean.getPatient(username);
-        else
-            return null;
+    public PatientDetails getPatientDetails( String username ) {
+        return patientFacadeBean.getPatient(username);
     }
-    public boolean addMeasurement(String username, String password, MeasurementDetails md) {
-        if(patientFacadeBean.isValid(username, password)) {
-            patientFacadeBean.addMeasurement(username, md);
-            return true;
-        }
-        else
-            return false;
+    public boolean addMeasurement(String username,  MeasurementDetails md) {
+         patientFacadeBean.addMeasurement(username, md);
+         return true;
     }
-
 }
-
-
-
